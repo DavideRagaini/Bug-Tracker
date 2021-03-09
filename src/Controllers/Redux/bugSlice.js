4e161f908bbe0c26bfafcb60309b535d5dcb1935
DavideRagaini <DavideRagaini@gmail.com>
2021-03-09 15:47:09 +0100

    removed what unecessary & basic structure & reducers

---
+++ src/Controllers/Redux/bugSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { retriveBugs } from "../bugController";
+
+const slice = createSlice({
+  name: "bug",
+  initialState: [],
+  reducer: {
+    getBugs: (state) => retriveBug(),
+    // getBugs: (state) => {},
+    createBugs: (state, actions) => {},
+    updateBugs: (state, actions) => {},
+    markComplete: (state, actions) => {},
+  },
+});
+
+export default slice.reducer;
+
+export const { getBugs, createBugs, updateBugs, markComplete } = slice.actions;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import App from "./App";
+
+// Reducers
+import authReducer from "./Controllers/Redux/authSlice";
+import bugReducer from "./controllers/Redux/bugSlice";
+import userReducer from "./controllers/Redux/userSlice";
+
+// Redux configure
+const rootReducer = combineReducers({
+  auth: authReducer,
+  bug: bugReducer,
+  user: userReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
